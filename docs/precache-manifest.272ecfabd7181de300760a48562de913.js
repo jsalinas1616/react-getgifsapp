f280302407e2373d12dbf301aa506a29e7d0069e
jsalinas1616 <jsalinas@odintec.com.mx>
2020-09-23 16:37:46 -0500

    docs agregado

---
+++ docs/precache-manifest.272ecfabd7181de300760a48562de913.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32cfa7fee5fca53f96622bec9397e983",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebac4c790211d2ce3f8b",
+    "url": "/static/css/main.e1c0b263.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebac4c790211d2ce3f8b",
+    "url": "/static/js/main.181fb188.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
